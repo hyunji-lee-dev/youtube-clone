06e0cd0627abffabb0339f0af108fb24ae2fe317
hyunji-lee-dev <hyunji.lee.dev@gmail.com>
2021-03-12 17:25:57 +0900

    Header 위치 고정

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,25 +12,27 @@ const Header = memo(({ onSubmit }) => {
 
   return (
     <header className={styles.header}>
-      <a href="http://localhost:3000/" className={styles.logo}>
-        <img src="images/logo.png" alt="logo" className={styles.logoImg} />
-        <span>YouTube</span>
-      </a>
-      <form className={styles.search} onSubmit={handleSubmit}>
-        <input
-          ref={inputRef}
-          type="text"
-          className={styles.searchInput}
-          placeholder="Search"
-        />
-        <button className={styles.searchBtn}>
-          <img
-            src="images/search.png"
-            alt="search button"
-            className={styles.btnImg}
+      <div className={styles.container}>
+        <a href="http://localhost:3000/" className={styles.logo}>
+          <img src="images/logo.png" alt="logo" className={styles.logoImg} />
+          <span>YouTube</span>
+        </a>
+        <form className={styles.search} onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            type="text"
+            className={styles.searchInput}
+            placeholder="Search"
           />
-        </button>
-      </form>
+          <button className={styles.searchBtn}>
+            <img
+              src="images/search.png"
+              alt="search button"
+              className={styles.btnImg}
+            />
+          </button>
+        </form>
+      </div>
     </header>
   );
 });
